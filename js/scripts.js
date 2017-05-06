502722010214156aa0f69c425b42538f1bcf1929
John Doe <john.doe@example.com>
2017-05-06 19:23:20 +0200

    js/scripts.js modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 
-var x = document.getElementsByClassName("button");
-	for (var i = 0; i < x.length; i++ ) {
-		var btn = x[i].innerText;
-		alert("Button: " + btn);
-	}
-console.log(btn);
+var x = document.getElementsByClassName('button');
+for (var i = 0; i < x.length; i++) {
+	var btn = x[i].innerText;
+	console.log('Button: ' + btn);
+}
